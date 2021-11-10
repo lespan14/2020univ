f03020059c5778cf99cf00c1325b3de10872061d
LesserPanda <44256398+lespan14@users.noreply.github.com>
2021-11-10 13:18:33 +0800

    Add files via upload
    2021_A1 backup

---
+++ comp2021/a1/BalancedBracketsTest.java
@@ -0,0 +1,44 @@
+package hk.edu.polyu.comp.comp2021.assignment1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BalancedBracketsTest {
+
+    @Test
+    public void test1() {
+        assertTrue(BalancedBrackets.isBalanced("()"));
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(BalancedBrackets.isBalanced("(){}"));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(BalancedBrackets.isBalanced("({})"));
+    }
+
+    @Test
+    public void test4() {
+        assertFalse(BalancedBrackets.isBalanced("(("));
+    }
+
+    @Test
+    public void test5() {
+        assertFalse(BalancedBrackets.isBalanced("({)}"));
+    }
+
+    @Test
+    public void test6() {
+        assertFalse(BalancedBrackets.isBalanced("(){"));
+    }
+
+    @Test
+    public void test7() {
+        assertTrue(BalancedBrackets.isBalanced("(()[{}])"));
+    }
+
+}
